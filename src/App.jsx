283a2833b2fc8d32b719a1010c5ecaa29127856c
Che <cheezy64@gmail.com>
2019-12-06 10:18:02 -0600

    component: add  and use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Header from './views/Header/Header';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        React
-      </header>
+    <div className='App'>
+      <Header title='Weirdness Calculator' />
+      <div className='content'>
+        <div className='content-left'>
+          content-left
+        </div>
+        <div className='content-right'>
+          content-right
+        </div>
+      </div>
     </div>
   );
 }
