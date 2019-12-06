899436442227551a3efcc59f8365f323364628de
Che <cheezy64@gmail.com>
2019-12-06 10:21:05 -0600

    component: add

---
+++ src/views/GifSearch/Search.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Search.css';
+
+const Search = ({
+  onSubmit,
+  onChange,
+  value,
+}) => {
+  return (
+    <div className='search'>
+      <form onSubmit={onSubmit}>
+        <label htmlFor='search'>Gif Search</label>
+        <input type='text' id='search' onChange={onChange} value={value} />
+        <button type='submit' className='btn btn-primary'>SEARCH</button>
+      </form>
+    </div>
+  );
+};
+
+Search.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+}
+
+export default Search;
