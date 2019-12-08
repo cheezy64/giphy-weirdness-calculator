2d8ffedfb6c4a40df8caab339025bddd38b91f58
Che <cheezy64@gmail.com>
2019-12-08 16:21:04 -0600

    image component: use fancy spinners

--- src/views/common/Image.jsx
+++ src/views/common/Image.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import { ClipLoader } from 'react-spinners';
+
+import './Image.css';
+
 const Image = ({ header, imageUrl, altText=header }) => {
   const [ isLoading, setIsLoading ] = useState(true);
   useEffect(() => {
@@ -10,11 +14,13 @@ const Image = ({ header, imageUrl, altText=header }) => {
   return (
     <div className='image-frame'>
       <p>{header}</p>
-      <div style={{ display: isLoading ? 'block' : 'none'}}>
-        Loading...
-      </div>
-      <div style={{ display: isLoading ? 'none' : 'block'}}>
-        <img src={imageUrl} alt={altText} onLoad={() => setIsLoading(false)} />
+      <div className='image'>
+        <div style={{ display: isLoading ? 'block' : 'none'}}>
+          <ClipLoader loading={isLoading} />
+        </div>
+        <div style={{ display: isLoading ? 'none' : 'block'}}>
+          <img src={imageUrl} alt={altText} onLoad={() => setIsLoading(false)} />
+        </div>
       </div>
     </div>
   )
