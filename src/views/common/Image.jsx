4ab15e4f6325e34c05ed02a763836f9b676a1215
Che <cheezy64@gmail.com>
2019-12-07 20:39:10 -0600

    Image: add loading placeholder

--- src/views/common/Image.jsx
+++ src/views/common/Image.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Image = ({ header, imageUrl, altText }) => {
-  const computedAltText = altText ? altText : header;
+const Image = ({ header, imageUrl, altText=header }) => {
+  const [ isLoading, setIsLoading ] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+  }, [imageUrl])
+
   return (
     <div className='image-frame'>
       <p>{header}</p>
-      <img src={imageUrl} alt={computedAltText} />
+      <div style={{ display: isLoading ? 'block' : 'none'}}>
+        Loading...
+      </div>
+      <div style={{ display: isLoading ? 'none' : 'block'}}>
+        <img src={imageUrl} alt={altText} onLoad={() => setIsLoading(false)} />
+      </div>
     </div>
   )
 };
