c399d5fbde3bca2ecb6f48de98a7e96a48d5f2e1
Che <cheezy64@gmail.com>
2019-12-08 00:16:32 -0600

    app: add react-router and tie in results

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import Header from './views/Header/Header';
 import SearchContainer from './views/GifSearch/SearchContainer';
 import SelectionContainer from './views/GifSelection/SelectionContainer';
+import WeirdnessCalculator from './views/WeirdnessCalculator/WeirdnessCalculator';
 
 import './App.css';
 
 function App({ reduxStore }) {
   return (
     <ReduxProvider store={reduxStore}>
-      <div className='App'>
-        <Header title='Weirdness Calculator' />
-        <div className='content'>
-          <div className='row'>
-            <div className='column'>
-              <SearchContainer />
-            </div>
-            <div className='column'>
-              <SelectionContainer />
-            </div>
+      <Router>
+        <div className='App'>
+          <Header title='Weirdness Calculator' />
+          <div className='content'>
+            <Switch>
+              <Route exact path='/'>
+                <div className='row'>
+                  <div className='column'>
+                    <SearchContainer />
+                  </div>
+                  <div className='column'>
+                    <SelectionContainer />
+                  </div>
+                </div>
+              </Route>
+              <Route path='/results' component={WeirdnessCalculator} />
+            </Switch>
           </div>
         </div>
-      </div>
+      </Router>
     </ReduxProvider>
   );
 }
--- src/views/GifSelection/SelectionContainer.jsx
+++ src/views/GifSelection/SelectionContainer.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useHistory } from "react-router-dom";
 
 import { REQUIRED_LIKES } from '../../config';
 
@@ -8,32 +9,34 @@ import Selection from './Selection';
 
 import { weirdnessCalculatorOperations } from '../../state/ducks/weirdnessCalculator';
 
-class SelectionContainer extends Component {
-  render() {
-    const { 
-      liked = [],
-      onUnlike,
-    } = this.props;
-    const remainingRequiredLikes = REQUIRED_LIKES - liked.length;
+const SelectionContainer = ({ liked = [], onUnlike }) => {
+  const remainingRequiredLikes = REQUIRED_LIKES - liked.length;
 
-    return (
-      <div className='selection-container'>
-        <h1>YOUR LIKED GIFS</h1>
-        <div className='selection-liked'>
-          {
-            liked.map(ele => 
-              <Selection 
-                imgHeader={ele.imgHeader}
-                imgUrl={ele.imgUrl}
-                onUnlike={ () => onUnlike(ele.imgHeader) } />
-            )
-          }
-        </div>
-        <button type='button' disabled={liked.length < REQUIRED_LIKES}>CALCULATE MY WEIRDNESS SCORE</button>
-        <p>{`You must like ${remainingRequiredLikes} more ${remainingRequiredLikes === 1 ? 'GIF' : 'GIFs'} to calculate your score`}</p>
+  const history = useHistory();
+
+  return (
+    <div className='selection-container'>
+      <h1>YOUR LIKED GIFS</h1>
+      <div className='selection-liked'>
+        {
+          liked.map(ele => 
+            <Selection 
+              imgHeader={ele.imgHeader}
+              imgUrl={ele.imgUrl}
+              onUnlike={ () => onUnlike(ele.imgHeader) } />
+          )
+        }
       </div>
-    )
-  }
+      <button 
+        type='button' 
+        disabled={liked.length < REQUIRED_LIKES}
+        onClick={() => history.push('/results')}
+        >
+          CALCULATE MY WEIRDNESS SCORE
+        </button>
+      <p>{`You must like ${remainingRequiredLikes} more ${remainingRequiredLikes === 1 ? 'GIF' : 'GIFs'} to calculate your score`}</p>
+    </div>
+  )
 };
 
 SelectionContainer.propTypes = {
